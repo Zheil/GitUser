7eab04a21e28f5f862d62711b3956eea679d953e
Ilya Zheil <ilya.zheil@gmail.com>
2018-12-20 18:54:21 +0200

    rename package

--- app/src/main/java/com/zheil/gituser/MainActivity.kt
+++ app/src/main/java/com/zheil/gituser/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zheil.di.gituser
+package com.zheil.gituser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.zheil.gituser.R
 
 class MainActivity : AppCompatActivity() {
 
