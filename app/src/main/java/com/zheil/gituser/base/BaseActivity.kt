d2d5437aab1a6c2dd8dd6fed2f2b3e23ec93b922
Ilya Zheil <ilya.zheil@gmail.com>
2018-12-20 19:03:32 +0200

    added base fragment, added replace, setup fragment

--- app/src/main/java/com/zheil/gituser/base/BaseActivity.kt
+++ app/src/main/java/com/zheil/gituser/base/BaseActivity.kt
@@ -2,14 +2,33 @@ package com.zheil.gituser.base
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 
 
 abstract class BaseActivity: AppCompatActivity() {
 
     protected abstract fun getLayoutResourceId(): Int
+    protected abstract fun getContainerResourceId(): Int
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutResourceId())
+    }
+
+    protected fun setupFragment(fragment: BaseFragment) {
+        val currentFragment: Fragment? = supportFragmentManager.findFragmentById(getContainerResourceId())
+        if (currentFragment == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(getContainerResourceId(), fragment)
+                    .commit()
+        }
+    }
+
+    protected fun replaceFragment(fragment: BaseFragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(getContainerResourceId(), fragment)
+                .commit()
     }
 }
