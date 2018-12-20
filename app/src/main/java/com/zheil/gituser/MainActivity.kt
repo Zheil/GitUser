76724846176d2862a0051cce35087761c2082448
Ilya Zheil <ilya.zheil@gmail.com>
2018-12-20 19:00:36 +0200

    implement base class to activity

--- app/src/main/java/com/zheil/gituser/MainActivity.kt
+++ app/src/main/java/com/zheil/gituser/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.zheil.gituser
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.zheil.gituser.R
+import com.zheil.gituser.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: BaseActivity() {
+    override fun getLayoutResourceId(): Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
